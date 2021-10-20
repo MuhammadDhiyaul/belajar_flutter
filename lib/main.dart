b21dff839904a02d03c9ce32b76e924379ef8a8a
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2021-10-20 23:07:03 +0700

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -56,26 +56,57 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Column Widget")),
-        body: Column(
-          children: [
+        appBar: AppBar(
+          title: Text("Contoh GridView Widget"),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
             Container(
-              color: Colors.purpleAccent,
-              child: FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.green,
+              alignment: Alignment.center,
+              child: Text("1",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
             ),
             Container(
-              color: Colors.greenAccent,
-              child: FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.red,
+              alignment: Alignment.center,
+              child: Text("2",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 400.0,
+              width: 300.0,
             ),
             Container(
-              color: Colors.orangeAccent,
-              child: FlutterLogo(
-                size: 90.0,
-              ),
+              color: Colors.deepPurple,
+              alignment: Alignment.center,
+              child: Text("3",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 200.0,
+              width: 200.0,
+            ),
+            Container(
+              color: Colors.blueGrey,
+              alignment: Alignment.center,
+              child: Text("4",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 200.0,
+              width: 200.0,
+            ),
+            Container(
+              color: Colors.cyan[800],
+              alignment: Alignment.center,
+              child: Text("5",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 200.0,
+              width: 200.0,
+            ),
+            Container(
+              color: Colors.yellow[800],
+              alignment: Alignment.center,
+              child: Text("6",
+                  style: TextStyle(fontSize: 30, color: Colors.white)),
+              height: 200.0,
+              width: 200.0,
             ),
           ],
         ),
